04a7147d25153dc534b6cba1768913e741e1c5ff
ykskb <yoheikusakabe@gmail.com>
2021-08-15 23:41:14 +0800

    Move duct files into dev resource directory.

--- dev/src/dev_duct.clj
+++ dev/src/dev_duct.clj
@@ -14,7 +14,7 @@
 (duct/load-hierarchy)
 
 (defn read-config []
-  (duct/read-config (io/resource "sapid/config.edn")))
+  (duct/read-config (io/resource "duct-config.edn")))
 
 (defn test []
   (eftest/run-tests (eftest/find-tests "test")))
